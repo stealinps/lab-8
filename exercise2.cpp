1b1f595648eed9ce1cf98d816a2c3067f8a2b739
Stealinp <stealinp@gmail.com>
2025-12-11 00:19:50 +0800

    Refactor distanceT function and improve formatting

--- exercise2.cpp
+++ exercise2.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 using namespace std;
-int distanceT(int t1, int t2){
-  int distance;
-  int table[6][6]={{0,256,43,475,876,623},{258,0,301,217,616,365},{43,301,0,518,918,667},{475,217,518,0,401,148},{876,616,918,401,0,246},{623,365,667,148,246,0}};
-  distance=table[t1][t2];
-  return distance;
+int distanceT(int t1, int t2) {
+	int distance;
+	int table[6][6] =
+	{ {0,256,43,475,876,623},
+	{256,0,301,217,616,365},
+	{43,301,0,518,918,667},
+	{475,217,518,0,401,148},
+	{876,616,918,401,0,246},
+	{623,365,667,148,246,0} };
+	distance = table[t1][t2];
+	return distance;
 }
-int main(){
-  int town1, town2;
-  cout << "Enter number for town 1:";
-  cin >> town1;
-  cout << "Enter number for town 2:";
-  cin >> town2;
-  cout << distanceT(town1,town2);
-  return 0;
+int main() {
+	int town1, town2;
+	cout << "Enter number for town 1:";
+	cin >> town1;
+	cout << "Enter number for town 2:";
+	cin >> town2;
+	cout << distanceT(town1, town2);
+	return 0;
 }
